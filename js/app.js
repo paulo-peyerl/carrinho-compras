2cd52e9874afdd3b98cff435c638cf743c7e04f4
paulo-peyerl <paulo.peyerl1@gmail.com>
2025-07-26 17:33:18 -0300

    Criando a lógica do carrinho

--- js/app.js
+++ js/app.js
@@ -9,4 +9,29 @@ function adicionar() {
         quantidade: quantidadeDeProdutos
     };
     carrinho.push(item);
+
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho() {
+    const lista = document.querySelector('#lista-produtos');
+    lista.innerHTML = '';
+    
+    let total = 0;
+
+    carrinho.forEach(item => {
+        const [nome, precoTexto] = item.produto.split('- R$');
+        const preco = Number(precoTexto);
+
+        const produtoEl = document.createElement('section');
+        produtoEl.className = 'carrinho__produtos__produto';
+        produtoEl.innerHTML = `<span class="texto-azul">${item.quantidade}X</span> ${nome} <span class="texto-azul">R$${preco}</span>`
+        
+        lista.appendChild(produtoEl);
+
+        total += preco * Number(item.quantidade);
+
+        document.getElementById('valor-total').textContent = `R$${total}`;
+    })
+
 }
